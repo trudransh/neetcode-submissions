012ed05cd941093c38e3fd41b6becf3c2fe8edce
Rudransh Singh Tomar <trudranshsingh2003@gmail.com>
2026-09-25 17:15:47 +0530

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numset = set(nums)
+        longest = 0
+
+        for i in numset:
+            #check if its a start of a sequence and check that by simply checking if it has a consecutive left number 
+            # we use set so that we can do O(1) lookup if we find a nuber with no left neighbour, we will move to check
+            # the right neighbourin present in the set, and we continue to do so untill we reach with noo right nos. 
+            # and we continue updating out longest number. 
+
+            if (i - 1) not in numset:
+                length = 0
+                while (i + length) in numset:
+                    length += 1
+                longest = max(length, longest)
+        return longest
+                
